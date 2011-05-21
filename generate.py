f5723e5e9218455d0729f56165d1f645f5252117
Matt Swanson <mdswanson@sep.com>
2011-05-20 21:52:42 -0400

    adding script to generate skeleton post

---
+++ generate.py
@@ -0,0 +1,30 @@
+from argparse import ArgumentParser
+import re
+from datetime import date
+
+p = ArgumentParser(description='Generate a jeykll blog post template')
+p.add_argument('title', help='title of the post, generates slug')
+p.add_argument('category', help='category (e.g blog, writeup)')
+args = p.parse_args()
+
+template = '''---
+layout: post
+title: %s
+categories:
+- %s
+---
+
+Go-go-gadget blog post
+'''
+
+def slugify(value):
+    "slugify non-crazy strings"
+    value = unicode(re.sub('[^\w\s-]', '', value).strip().lower())
+    return re.sub('[-\s]+', '-', value)
+
+fn = str(date.today()) + '-' + slugify(args.title) + '.md'
+post = template % (args.title, args.category)
+
+with open('_posts/%s' % fn, 'w') as fp:
+    fp.write(post)
+print "generated %s" % fn
